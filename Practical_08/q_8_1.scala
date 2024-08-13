1e05ae3b0164773e1f169914d5c5421f7dfa223c
Pawan Weerasuriya <uwpawan@gmail.com>
2024-08-13 09:32:57 +0530

    Update q_8_1.scala

--- Practical_08/q_8_1.scala
+++ Practical_08/q_8_1.scala
@@ -1,1 +1,27 @@
-This file should be completed!
+object q_8_1{
+    
+    def encrypt(text: String , shifter: Int): String = {
+        text.map(c => (c + shifter).toChar)
+    }
+
+    def decrypt(text: String , shifter: Int): String = {
+        text.map(c => (c - shifter).toChar)
+    }
+
+    def cipher(text: String, shift: Int, algo: (String, Int) => String): String = {
+        algo(text, shift)
+    }
+
+    def main(args: Array[String]): Unit = {
+        val originaltext = "Hi, Pawan!"
+        val shift = 1
+
+        // Encrypt the originaltext
+        val encryptedText = cipher(originaltext, shift, encrypt)
+        println(s"Encrypted Text: $encryptedText")
+
+        // Decrypt the ciphertext
+        val decryptedText = cipher(encryptedText, shift, decrypt)
+        println(s"Decrypted Text: $decryptedText")
+    }
+}
