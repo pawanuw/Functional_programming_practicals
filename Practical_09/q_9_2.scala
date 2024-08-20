b5e85c00948599870093dafa7e78cc808125d3ac
Pawan Weerasuriya <uwpawan@gmail.com>
2024-08-20 10:31:37 +0530

    q_9_2.scala

--- Practical_09/q_9_2.scala
+++ Practical_09/q_9_2.scala
@@ -1,15 +1,20 @@
-object q_9_1 extends App {
-    def func_A():={
-        
+object question2 {
+
+  def main(args: Array[String]): Unit = {
+    println("Enter the integer :")
+
+    val input = scala.io.StdIn.readLine()
+    val value = input.toInt
+
+    integer(value)
+  }
+
+  def integer(value: Int): Unit = {
+    value match {
+      case d if d <= 0 => println("Negative/Zero")
+      case d if d % 2 == 0 => println("Even number")
+      case d if d % 2 != 0 => println("Odd number")
+      case _ => println("invalid value")
     }
-    def function_decider(depo_amount:Double):Int = depo_amount match {
-        case x if x < 0 => 0
-        case x if x < 20000 => 1
-        case x if x < 200000 => 2
-        case x if x < 2000000 => 3
-        case _ => 4
-    }
-    def interest(depo_amount:Double,):Double = {
-        
-    }
+  }
 }
